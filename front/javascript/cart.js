fdd586e56ca4b49c5cbef2115d295db7b4773050
zoukari <zoukari20@gmail.com>
2022-06-13 17:11:08 +0200

    changement panier

--- front/javascript/cart.js
+++ front/javascript/cart.js
@@ -3,28 +3,22 @@
 const itemsSection = document.getElementById("items");
 const url = "http://localhost:3000/api/products"
 
-
 const basketDataDisplay = document.getElementById("cart__items");
 let basketData
 
 fetch(url)
     .then(response => response.json())
     .then(prouctsDatas => prouctsDatas.filter((item) => {
-
         basketData = item
-
         //var basketItemsStorage= localStorage.getItem('basketItemId')
         let basketDatasStorage = localStorage.getItem('basketDataItems')
 
         let datason = JSON.parse(basketDatasStorage)
         datason.filter((item2) => {
             let item_id_ = item2.id
-            if (item_id_.includes(item._id)) {
-
-
+            if(item_id_.includes(item._id)) {
                 basketDataDisplay.innerHTML += `
-
-    <article class="cart__item" data-id="${item._id}" data-color="{product-color}">
+    <article class="cart__item" data-id="${item._id}" data-color="${item2.color}">
     <div class="cart__item__img">
       <img src="${basketData.imageUrl}" alt="${item.altTxt}">
     </div>
@@ -46,18 +40,21 @@ fetch(url)
     </div>
     </article> 
     
-    
     `;
-
-
             }
         })
-
-
-
-
     }))
 
+
+    function ValidateEmail(mail) 
+    {
+     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+      {
+        return (true)
+      }
+       // alert("You have entered an invalid email address!")
+        return (false)
+    }
 
 
 /*  commander */
@@ -65,8 +62,6 @@ let order_url = "http://localhost:3000/api/products/order"
 setTimeout(function() {
     const order = document.getElementById("order");
     order.addEventListener("click", function(e) {
-
-
 
         e.preventDefault()
         /*get input and user data*/
@@ -107,10 +102,6 @@ setTimeout(function() {
         stringifyedPayload_ = payload
         stringifyedPayload_.products = array_item5.split(/[.,!,?,;,...]/);
         console.log(stringifyedPayload_)
-
-
-
-
         let options = {
             method: "POST",
             headers: {
@@ -125,42 +116,67 @@ setTimeout(function() {
             })
             .then((data) => {
 
-
+                console.log(order_url)
 
                 //validation form
 
-                if (user_firstName == "") {
+             //validation form
+             let validateFirstName = validateLastname = validateAdress = validateCity = validateEmail = false
+                     
+             if (user_firstName == "") {
+                document.getElementById('firstNameErrorMsg').innerHTML = "Veuillez entrer votre nom"
 
-                    alert("Veuillez entrer votre nom")
-                } else if (user_lastName == "") {
+            }else{
+                document.getElementById('firstNameErrorMsg').innerHTML = ""
+                validateFirstName = true
+            }
+            
+             if (user_lastName == "") {
+                document.getElementById('lastNameErrorMsg').innerHTML = "Veuillez entrer votre prenom"
 
-                    alert("Veuillez entrer votre prenom")
-                } else if (user_address == "") {
+            }  else{
+                document.getElementById('lastNameErrorMsg').innerHTML = ""
+                validateLastname = true
 
-                    alert("Veuillez entrer votre adresse")
-                } else if (user_city == "") {
+            }
+             if (user_address == "") {
+                document.getElementById('addressErrorMsg').innerHTML = "Veuillez entrer votre addresse"
 
-                    alert("Veuillez entrer votre ville")
-                } else if (user_email == "") {
+            } else{
+                document.getElementById('addressErrorMsg').innerHTML = ""
+                validateAdress = true
+            }
+            
+             if (user_city == "") {
+                document.getElementById('cityErrorMsg').innerHTML = "Veuillez entrer votre ville"
 
-                    alert("Veuillez entrer votre mail")
-                } else {
+            }else{
+                document.getElementById('cityErrorMsg').innerHTML = ""
+                validateCity = true
+            }
+            
+             if (user_email == "" ) {
 
-                    window.location.href = "../html/confirmation.html?order-id=" + data.orderId;
-                    localStorage.clear();
+                document.getElementById('emailErrorMsg').innerHTML = "Veuillez entrer votre mail"
 
-
-
-                }
-
-
-
-
+               
+            } 
+            else if (!ValidateEmail(user_email)){
+                document.getElementById('emailErrorMsg').innerHTML = "Votre Email est incorrect"
+            
+            }
+            
+            else{
+                document.getElementById('emailErrorMsg').innerHTML = ""
+                validateEmail = true
+            }
+            console.log('valise' + validateFirstName + ' ' + validateLastname + ' ' + validateAdress + ' ' + validateCity + ' ' + validateEmail)
+            if(validateFirstName && validateLastname && validateAdress && validateCity && validateEmail) {
+                window.location.href = "../html/confirmation.html?order-id=" + data.orderId;
+                localStorage.clear();
+            }
                 console.log(data)
             });
-
-
-
     })
 }, 1000)
 
@@ -171,6 +187,7 @@ setTimeout(function() {
 
 let total = 0
 let updatedTotal = 0
+
 
 function displayTotalToPay() {
     fetch(url)
@@ -202,42 +219,47 @@ function displayTotalToPay() {
 }
 displayTotalToPay()
 
-
-
-
 //delete product from basket
 setTimeout(function() {
+
     const deletProductBtn = document.getElementsByClassName("deleteItem")
     for (i = 0; i < deletProductBtn.length; i++) {
 
         deletProductBtn[i].addEventListener("click", function(event) {
-
-
-
-
+          
             let getOldBasketItemId = JSON.parse(localStorage.getItem("basketItemId"));
             let getOldbasketDataItems = JSON.parse(localStorage.getItem("basketDataItems"));
-
-            let rest_basketItemId = getOldBasketItemId.filter((data) => {
-                const data_set_delete = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-id")
-
-                return data !== data_set_delete
-            })
+            let data_set_delete = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-id")
+            let data_color = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-color")
 
             let rest_getOldbasketDataItems = getOldbasketDataItems.filter((data) => {
-                let data_set_delete = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-id")
+              
+              let countOtherItems = 0
+                const otherItems =  getOldbasketDataItems.filter(item => {
+                               if(item.id == data_set_delete){
+                                countOtherItems += 1
+                               } 
+                            })
+        if(countOtherItems <= 1){
 
-                return data.id !== data_set_delete
+            let rest_basketItemId = getOldBasketItemId.filter((data) => {
+              
+
+               return data !== data_set_delete
             })
 
-
-
-
             localStorage.setItem('basketItemId', JSON.stringify(rest_basketItemId));
+
+        }
+                return data.color !== data_color 
+                
+                
+            })
+           // 
             localStorage.setItem('basketDataItems', JSON.stringify(rest_getOldbasketDataItems));
+            //event.preventDefault();
 
-
-            window.location.href = "../html/cart.html"
+          window.location.href = "../html/cart.html"
         })
 
 
@@ -262,16 +284,10 @@ setTimeout(function() {
 
             let getOldbasketDataItems = JSON.parse(localStorage.getItem("basketDataItems"));
 
-
-
-
             let oldbasketDataItems = JSON.parse(localStorage.getItem("basketDataItems"));
             let itemId = event.target.parentElement.parentElement.parentElement.parentElement.getAttribute("data-id")
 
             let newProductNumber = event.target.value
-
-
-
 
             oldbasketDataItems.filter((data) => {
 
@@ -294,10 +310,7 @@ setTimeout(function() {
                         oldbasketDataItemsNew.push(SendNewObject)
                         localStorage.setItem('basketDataItems', JSON.stringify(oldbasketDataItemsNew));
 
-
                         setTimeout(function() {
-
-
 
                             let arr_ = []
                             fetch(url)
--- front/javascript/product.js
+++ front/javascript/product.js
@@ -1,35 +1,31 @@
 
 const itemsSection = document.getElementById("items");
-const url = "http://localhost:3000/api/products"
-
-
+const url = "http://localhost:3000/api/products/"
 
 const url_ = window.location.href
 const strs = url_.split('=');
 const product_id = strs.at(-1)
 
 let display_data;
- const single_product = document.getElementById("product_display");
+const single_product = document.getElementById("product_display");
 
-fetch(url)
+let uniqueProduct = url + product_id
+
+fetch(uniqueProduct)
   .then(response => response.json())
-  .then(prouctsDatas => prouctsDatas.filter((item) => {
-   
-    if(item._id === product_id){
-        display_data = item
-         
-        
-            setTimeout(() => {
-                const color_list = document.getElementById("colors")
+  .then(prouctsDatas => {
+    if (prouctsDatas._id === product_id) {
+      display_data = prouctsDatas
+      console.log(prouctsDatas)
+      setTimeout(() => {
+        const color_list = document.getElementById("colors")
 
-                item.colors.forEach((element, index) => {
-                    color_list.innerHTML +=  `<option value="${index}">${element}</option>`
-                })
-            }, 100);
-          
-    
+        prouctsDatas.colors.forEach((element, index) => {
+          color_list.innerHTML += `<option value="${index}">${element}</option>`
+        })
+      }, 100);
 
-product_display.innerHTML = `
+      product_display.innerHTML = `
 
 <article>
 <div class="item__img">
@@ -51,34 +47,26 @@ product_display.innerHTML = `
     <div class="item__content__settings__color">
       <label for="color-select">Choisir une couleur :</label>
       <select name="color-select" id="colors">
-
-         
-         
-
-
       </select>
     </div>
-
     <div class="item__content__settings__quantity">
       <label for="itemQuantity">Nombre d'article(s) (1-100) :</label>
-      <input type="number" name="itemQuantity" min="1" max="100" value="0" id="quantity">
+      <input type="number" name="itemQuantity" min="1" max="100" value="1" id="quantity">
     </div>
   </div>
-
   <div class="item__content__addButton">
     <button id="addToCart">Ajouter au panier</button>
   </div>
 
 </div>
 </article>
-
-
 `;
 
-    } 
-       
-       
-}))
+    } else {
+      document.write('PAS DE PRODUIT')
+    }
+
+  })
 
 
 
@@ -86,60 +74,95 @@ product_display.innerHTML = `
 
 
 
-if(localStorage.getItem('basketItemId') == null){
-    localStorage.setItem('basketItemId', '[]'); 
-  
-  }
-  
-  
-  if(localStorage.getItem('basketDataItems') == null){
-    localStorage.setItem('basketDataItems', '[]'); 
-  
-  } 
-  
-  
-  
-  let url2 = "http://localhost:3000/api/products/order"
-  setTimeout(function(){
-    const addToCart = document.getElementById("addToCart");
-    addToCart.addEventListener("click", function(){
-  
-  
-  
-    
+if (localStorage.getItem('basketItemId') == null) {
+  localStorage.setItem('basketItemId', '[]');
+
+}
+
+
+if (localStorage.getItem('basketDataItems') == null) {
+  localStorage.setItem('basketDataItems', '[]');
+
+}
+
+
+
+let url2 = "http://localhost:3000/api/products/order"
+setTimeout(function () {
+  const addToCart = document.getElementById("addToCart");
+  addToCart.addEventListener("click", function () {
+
+    const colors_selected = document.getElementById("colors")
+    const color_selected = colors_selected.options[colors_selected.selectedIndex].text;
+    const quantity_selected = document.getElementById("quantity").value
+
+
+    let oldBasketItemId = JSON.parse(localStorage.getItem("basketItemId"));
+    let oldbasketDataItems = JSON.parse(localStorage.getItem("basketDataItems"));
+    let oldbasketDataItemsString = JSON.stringify(oldbasketDataItems)
+
+    // const addSameProduct = oldbasketDataItemsString.filter(element => element.id = product_id );
+
+
+    if (oldBasketItemId.includes(product_id) === false) {
+
+
+
+
+      oldBasketItemId.push(product_id);
+      oldbasketDataItems.push({ "color": color_selected, "productNumber": quantity_selected, "id": product_id });
+
+
+      localStorage.setItem('basketItemId', JSON.stringify(oldBasketItemId));
+      localStorage.setItem('basketDataItems', JSON.stringify(oldbasketDataItems));
+
+
+
+      alert("Le produit a ete ajoute au panier 1")
+
+
+
+    } else if(oldBasketItemId.includes(product_id) === true) { 
+let variant = false
      
-  
-  
+      oldbasketDataItems.filter((item2) => { 
+        const color_selectedUpt = colors_selected.options[colors_selected.selectedIndex].text;
+
+        oldData = JSON.stringify(oldbasketDataItems)
+
+       // console.log('other' + item2.color + ' is differant of ' + color_selected + 'and ' + item2.id + 'is equal to ' + product_id)
+        if (item2.color === color_selectedUpt && item2.id == product_id)
+        {   
+          return false
+          console.log(item2.color  + ' we dont add again..,' )
       
-        const colors_selected = document.getElementById("colors")
-        const color_selected = colors_selected.options[colors_selected.selectedIndex].text;
-        const quantity_selected = document.getElementById("quantity").value 
-  
       
-       let oldBasketItemId = JSON.parse(localStorage.getItem("basketItemId")); 
-       let oldbasketDataItems = JSON.parse(localStorage.getItem("basketDataItems")); 
-  
-  
-       if (oldBasketItemId.includes(product_id) === false){
-       
-  
-  
-          oldBasketItemId.push(product_id);
-          oldbasketDataItems.push({"color":color_selected, "productNumber": quantity_selected, "id":product_id});
-          
-  
-          alert("Le produit a ete ajoute au panier")
-        }else{
-          alert("Le produit existe deja dans le panier")
-  
-        }
-      
-       localStorage.setItem('basketItemId', JSON.stringify(oldBasketItemId));
-       localStorage.setItem('basketDataItems', JSON.stringify(oldbasketDataItems));
-    
-      
-     
-  
-  
-    })
-  }, 2000)
+      }else if(item2.color !== color_selectedUpt && item2.id === product_id){
+        console.log(' we should add again..' + 'color is ' + color_selectedUpt + " vs " + item2.color)
+        variant = true 
+        console.log(variant)
+
+
+    oldbasketDataItems.push({ "color": color_selectedUpt, "productNumber": quantity_selected, "id": product_id });
+
+function getUniqueListBy(arr, key) {
+  return [...new Map(arr.map(item => [item[key], item])).values()]
+}
+
+const updateOldbasketDataItems = getUniqueListBy(oldbasketDataItems, 'color')
+localStorage.setItem('basketDataItems', JSON.stringify(updateOldbasketDataItems)); 
+
+
+      }
+   
+      }) 
+
+      if(variant){
+        console.log('here is' + variant)
+      }
+
+
+    }
+
+  })
+}, 2000)
